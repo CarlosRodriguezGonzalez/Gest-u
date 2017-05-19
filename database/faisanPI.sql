48ff1698f8a970038154c5ec9e130b9480a4f4e0
carlosRodriguez <carlosrg1997@hotmail.com>
2017-05-19 09:53:31 +0200

    SQL

--- database/faisanPI.sql
+++ database/faisanPI.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
--- Servidor: localhost
--- Tiempo de generación: 19-05-2017 a las 02:26:16
--- Versión del servidor: 10.1.21-MariaDB
--- Versión de PHP: 7.1.1
+-- Host: 127.0.0.1
+-- Generation Time: May 19, 2017 at 09:45 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -16,14 +16,18 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+DROP DATABASE IF EXISTS FaisanPI;
+CREATE DATABASE FaisanPI;
+USE FaisanPI;
+
 --
--- Base de datos: `faisanPI`
+-- Database: `faisanpi`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `actividad`
+-- Table structure for table `actividad`
 --
 
 CREATE TABLE `actividad` (
@@ -37,16 +41,24 @@ CREATE TABLE `actividad` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `actividad`
+-- Dumping data for table `actividad`
 --
 
 INSERT INTO `actividad` (`id_actividad`, `fecha`, `tipo`, `nombre_actividad`, `lugar`, `destinatario`, `descripcion`) VALUES
-(1, '22-04-2017', 'Ocio', 'Gymkana', 'IES Carmen Martin Gaite', NULL, 'Gymkana con distintos circuitos orientada a niños.');
+(1, '22-04-2017', 'Ocio', 'Gymkana', 'IES Carmen Martin Gaite', NULL, 'Gymkana con distintos circuitos orientada a niños.'),
+(2, '10-11-2011', 'Ocio', 'Todo por ti ', 'Pueblo nuevo', NULL, 'Actividades para jovenes entre 3-12 años'),
+(3, '15-6-2007', 'Deporte', 'Charly fit', 'Barrio Salamanca', NULL, 'Charly te entrena(o no)'),
+(4, '25-6-2013', 'Ocio', 'Jonyou', 'Campo de arriba', NULL, 'ocio y tiempo libre'),
+(5, '14-5-2010', 'Deporte', 'Bumbum', 'IES Juan de Herrera', NULL, 'Competición de petanca'),
+(6, '4-6-2012', 'Deporte', 'El tutú', 'Barrio el Padrón', NULL, 'Competición de carreras de saco'),
+(7, '22-8-2013', 'Ocio', 'PEPETE', 'Barrio Maria Luisa', NULL, 'Venta de antiguedades'),
+(8, '5-6-2013', 'Ocio', 'EL muni', 'Barrio viejo', NULL, 'Curso de informática gratuito'),
+(9, '2-10-2013', 'Ocio', 'Cosika', 'Parque Grande', NULL, 'Curso de carpintería');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `actividad_tipo`
+-- Table structure for table `actividad_tipo`
 --
 
 CREATE TABLE `actividad_tipo` (
@@ -57,7 +69,7 @@ CREATE TABLE `actividad_tipo` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `asociacion`
+-- Table structure for table `asociacion`
 --
 
 CREATE TABLE `asociacion` (
@@ -81,12 +93,12 @@ CREATE TABLE `asociacion` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `asociacion`
+-- Dumping data for table `asociacion`
 --
 
 INSERT INTO `asociacion` (`id_asociacion`, `nombre`, `cif`, `direccion`, `municipio`, `cp`, `telef_fijo`, `telef_movil`, `fax`, `email`, `baja`, `estatutos`, `num_registro`, `acta_junta`, `cert_domicilio`, `fecha_solicitud`, `id_representante`) VALUES
 (1, 'Tiro al plato Faisan', 'B876234G', 'C/Jacinto Benavente P2 N4', 'Las Rozas', 28232, 918543234, 681019232, NULL, 'tiroplatofaisan@faisanworld.com', 1, 1, 1, 1, 1, '15-04-2015', 0),
-(2, 'We\'re Quiros', 'B432243C', 'C/ Poniente 28', 'Moralzarzal', 28411, 918432345, 681012343, NULL, 'werequiros@gmail.com', 1, 1, 1, 1, 1, '04-09-2016', 0),
+(2, 'We''re Quiros', 'B432243C', 'C/ Poniente 28', 'Moralzarzal', 28411, 918432345, 681012343, NULL, 'werequiros@gmail.com', 1, 1, 1, 1, 1, '04-09-2016', 0),
 (3, 'Fundacion UCA', 'B33423234V', 'C/ Daniel de la Uca 23', 'Mahadaonda', 28413, 918432121, 671221212, NULL, 'daniglesiauca@hotmail.es', 1, 1, 1, 1, 1, '25-02-2017', 0),
 (4, 'Fundacion Ochentera', 'B334232V', 'C/ Elfo P1 4', 'Madrid', 28022, 917645321, 3434, NULL, 'fundacionochentera@gmail.com', 1, 1, 1, 1, 1, '28-02-2017', 0),
 (5, 'Loleros Anónimos', 'B456723C', 'Avda. Grieta del invocador', 'Runaterra', 28023, 98789213, 3434, NULL, 'lolerosanonimos@hotmail.com', 1, 1, 1, 1, 1, '01-03-2017', 0),
@@ -104,7 +116,7 @@ INSERT INTO `asociacion` (`id_asociacion`, `nombre`, `cif`, `direccion`, `munici
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `asociacion_actividad_organiza`
+-- Table structure for table `asociacion_actividad_organiza`
 --
 
 CREATE TABLE `asociacion_actividad_organiza` (
@@ -115,7 +127,7 @@ CREATE TABLE `asociacion_actividad_organiza` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `asociacion_actividad_participa`
+-- Table structure for table `asociacion_actividad_participa`
 --
 
 CREATE TABLE `asociacion_actividad_participa` (
@@ -126,7 +138,7 @@ CREATE TABLE `asociacion_actividad_participa` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `asociacion_tipo`
+-- Table structure for table `asociacion_tipo`
 --
 
 CREATE TABLE `asociacion_tipo` (
@@ -137,7 +149,7 @@ CREATE TABLE `asociacion_tipo` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `representante`
+-- Table structure for table `representante`
 --
 
 CREATE TABLE `representante` (
@@ -154,7 +166,7 @@ CREATE TABLE `representante` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `representante`
+-- Dumping data for table `representante`
 --
 
 INSERT INTO `representante` (`id_representante`, `nombre`, `NIF_NIE`, `direccion`, `municipio`, `codigo_postal`, `tfn_fijo`, `tfn_movil`, `fax`, `email`) VALUES
@@ -163,7 +175,7 @@ INSERT INTO `representante` (`id_representante`, `nombre`, `NIF_NIE`, `direccion
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `sede`
+-- Table structure for table `sede`
 --
 
 CREATE TABLE `sede` (
@@ -175,16 +187,21 @@ CREATE TABLE `sede` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `sede`
+-- Dumping data for table `sede`
 --
 
 INSERT INTO `sede` (`id_sede`, `direccion`, `caracteristicas`, `fecha_solicitud`, `fecha_concesion`) VALUES
-(1, 'C/ De la Piruleta', 'Duplex amplio', '05-04-2017', '07-04-2017');
+(1, 'C/ De la Piruleta', 'Duplex amplio', '05-04-2017', '07-04-2017'),
+(2, 'C/ Velazquez', 'Pradera', '5-7-2013', '22-7-2013'),
+(3, 'C/ Perez', 'Local', '12-8-2013', '5-9-2013'),
+(4, 'C/ Alvarez', 'Local', '4-7-2014', '26-10-2014'),
+(5, 'C/ Pepon', 'Parcela', '3-2-2016', '2-5-2015'),
+(6, 'C/ Alfredo', 'Chalet', '3-4-2016', '22-5-2016');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `sede_asociacion`
+-- Table structure for table `sede_asociacion`
 --
 
 CREATE TABLE `sede_asociacion` (
@@ -195,7 +212,7 @@ CREATE TABLE `sede_asociacion` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `subvencion`
+-- Table structure for table `subvencion`
 --
 
 CREATE TABLE `subvencion` (
@@ -206,16 +223,24 @@ CREATE TABLE `subvencion` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
--- Volcado de datos para la tabla `subvencion`
+-- Dumping data for table `subvencion`
 --
 
 INSERT INTO `subvencion` (`id_solicitud`, `fecha_solicitud`, `importe`, `id_actividad`) VALUES
-(1, '02-05-2017', 567, 1);
+(1, '02-05-2017', 567, 1),
+(2, '22-6-2015', 1020, 2),
+(3, '21-6-2012', 2000, 3),
+(4, '5-6-2015', 520, 4),
+(5, '2-5-2016', 1300, 3),
+(6, '7-6-2017', 200, 5),
+(7, '23-8-2017', 500, 6),
+(8, '22-9-2017', 3000, 8),
+(9, '6-8-2017', 800, 9);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tipo`
+-- Table structure for table `tipo`
 --
 
 CREATE TABLE `tipo` (
@@ -224,153 +249,153 @@ CREATE TABLE `tipo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `actividad`
+-- Indexes for table `actividad`
 --
 ALTER TABLE `actividad`
   ADD PRIMARY KEY (`id_actividad`);
 
 --
--- Indices de la tabla `actividad_tipo`
+-- Indexes for table `actividad_tipo`
 --
 ALTER TABLE `actividad_tipo`
   ADD PRIMARY KEY (`id_actividad`,`id_tipo`),
   ADD KEY `fk_tipo_actividad` (`id_tipo`);
 
 --
--- Indices de la tabla `asociacion`
+-- Indexes for table `asociacion`
 --
 ALTER TABLE `asociacion`
   ADD PRIMARY KEY (`id_asociacion`),
   ADD KEY `fk_aso_repre` (`id_representante`);
 
 --
--- Indices de la tabla `asociacion_actividad_organiza`
+-- Indexes for table `asociacion_actividad_organiza`
 --
 ALTER TABLE `asociacion_actividad_organiza`
   ADD PRIMARY KEY (`id_asociacion`,`id_actividad`),
   ADD KEY `fk_actividad_asociacion_organiza` (`id_actividad`);
 
 --
--- Indices de la tabla `asociacion_actividad_participa`
+-- Indexes for table `asociacion_actividad_participa`
 --
 ALTER TABLE `asociacion_actividad_participa`
   ADD PRIMARY KEY (`id_asociacion`,`id_actividad`),
   ADD KEY `fk_actividad_asociacion_participa` (`id_actividad`);
 
 --
--- Indices de la tabla `asociacion_tipo`
+-- Indexes for table `asociacion_tipo`
 --
 ALTER TABLE `asociacion_tipo`
   ADD PRIMARY KEY (`id_asociacion`,`id_tipo`),
   ADD KEY `fk_tipo_asociacion` (`id_tipo`);
 
 --
--- Indices de la tabla `representante`
+-- Indexes for table `representante`
 --
 ALTER TABLE `representante`
   ADD PRIMARY KEY (`id_representante`);
 
 --
--- Indices de la tabla `sede`
+-- Indexes for table `sede`
 --
 ALTER TABLE `sede`
   ADD PRIMARY KEY (`id_sede`);
 
 --
--- Indices de la tabla `sede_asociacion`
+-- Indexes for table `sede_asociacion`
 --
 ALTER TABLE `sede_asociacion`
   ADD PRIMARY KEY (`id_asociacion`,`id_sede`),
   ADD KEY `fk_sede_aso` (`id_sede`);
 
 --
--- Indices de la tabla `subvencion`
+-- Indexes for table `subvencion`
 --
 ALTER TABLE `subvencion`
   ADD PRIMARY KEY (`id_solicitud`),
   ADD KEY `fk_solicitud_actividad` (`id_actividad`);
 
 --
--- Indices de la tabla `tipo`
+-- Indexes for table `tipo`
 --
 ALTER TABLE `tipo`
   ADD PRIMARY KEY (`id_tipo`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `actividad`
+-- AUTO_INCREMENT for table `actividad`
 --
 ALTER TABLE `actividad`
-  MODIFY `id_actividad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_actividad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
--- AUTO_INCREMENT de la tabla `asociacion`
+-- AUTO_INCREMENT for table `asociacion`
 --
 ALTER TABLE `asociacion`
   MODIFY `id_asociacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
--- AUTO_INCREMENT de la tabla `sede`
+-- AUTO_INCREMENT for table `sede`
 --
 ALTER TABLE `sede`
-  MODIFY `id_sede` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_sede` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
--- AUTO_INCREMENT de la tabla `subvencion`
+-- AUTO_INCREMENT for table `subvencion`
 --
 ALTER TABLE `subvencion`
-  MODIFY `id_solicitud` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_solicitud` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `actividad_tipo`
+-- Constraints for table `actividad_tipo`
 --
 ALTER TABLE `actividad_tipo`
   ADD CONSTRAINT `fk_actividad_tipo` FOREIGN KEY (`id_actividad`) REFERENCES `actividad` (`id_actividad`),
   ADD CONSTRAINT `fk_tipo_actividad` FOREIGN KEY (`id_tipo`) REFERENCES `tipo` (`id_tipo`);
 
 --
--- Filtros para la tabla `asociacion`
+-- Constraints for table `asociacion`
 --
 ALTER TABLE `asociacion`
   ADD CONSTRAINT `fk_aso_repre` FOREIGN KEY (`id_representante`) REFERENCES `representante` (`id_representante`);
 
 --
--- Filtros para la tabla `asociacion_actividad_organiza`
+-- Constraints for table `asociacion_actividad_organiza`
 --
 ALTER TABLE `asociacion_actividad_organiza`
   ADD CONSTRAINT `fk_actividad_asociacion_organiza` FOREIGN KEY (`id_actividad`) REFERENCES `actividad` (`id_actividad`),
   ADD CONSTRAINT `fk_asociacion_actividad_organiza` FOREIGN KEY (`id_asociacion`) REFERENCES `asociacion` (`id_asociacion`);
 
 --
--- Filtros para la tabla `asociacion_actividad_participa`
+-- Constraints for table `asociacion_actividad_participa`
 --
 ALTER TABLE `asociacion_actividad_participa`
   ADD CONSTRAINT `fk_actividad_asociacion_participa` FOREIGN KEY (`id_actividad`) REFERENCES `actividad` (`id_actividad`),
   ADD CONSTRAINT `fk_asociacion_actividad_participa` FOREIGN KEY (`id_asociacion`) REFERENCES `asociacion` (`id_asociacion`);
 
 --
--- Filtros para la tabla `asociacion_tipo`
+-- Constraints for table `asociacion_tipo`
 --
 ALTER TABLE `asociacion_tipo`
   ADD CONSTRAINT `fk_asociacion_tipo` FOREIGN KEY (`id_asociacion`) REFERENCES `asociacion` (`id_asociacion`),
   ADD CONSTRAINT `fk_tipo_asociacion` FOREIGN KEY (`id_tipo`) REFERENCES `tipo` (`id_tipo`);
 
 --
--- Filtros para la tabla `sede_asociacion`
+-- Constraints for table `sede_asociacion`
 --
 ALTER TABLE `sede_asociacion`
   ADD CONSTRAINT `fk_aso_sede` FOREIGN KEY (`id_asociacion`) REFERENCES `asociacion` (`id_asociacion`),
   ADD CONSTRAINT `fk_sede_aso` FOREIGN KEY (`id_sede`) REFERENCES `sede` (`id_sede`);
 
 --
--- Filtros para la tabla `subvencion`
+-- Constraints for table `subvencion`
 --
 ALTER TABLE `subvencion`
   ADD CONSTRAINT `fk_solicitud_actividad` FOREIGN KEY (`id_actividad`) REFERENCES `actividad` (`id_actividad`);
